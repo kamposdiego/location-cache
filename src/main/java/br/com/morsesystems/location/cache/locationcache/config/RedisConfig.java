5ab9517ef9876ce1fd672938049fb0531bb1a5c1
Diego Mendes Ferreira Campos <diego.fcampos@dataprev.gov.br>
2023-02-24 11:50:40 -0300

    Init commit

--- src/main/java/br/com/morsesystems/location/cache/locationcache/config/RedisConfig.java
+++ src/main/java/br/com/morsesystems/location/cache/locationcache/config/RedisConfig.java
@@ -1,11 +1,16 @@
 package br.com.morsesystems.location.cache.locationcache.config;
 
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 
 @Configuration
-@EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("redis-service", 6379));
+    }
 
 }
